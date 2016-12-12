67ff821a38c241bedd28a16f8fa78200a1efdc3f
Duncan Garland <duncan.garland@ntlworld.com>
2016-12-12 17:35:57 +0000

    Scoreboard labels are now different between desktop and mobile where necessary. Single extras can now be added.

--- angular-prototype/public/javascripts/controllers/ScorerController.js
+++ angular-prototype/public/javascripts/controllers/ScorerController.js
@@ -24,10 +24,20 @@ angular.module('scorer', []).controller('ScorerController', function($scope) {
       no: 7,
       striker: true,
       runs: 20
-    }
+    },
+    game_over: false,
+    extras: 0
   };
 
-  var change_ends = function() {
+  var change_ends = function(num_runs) {
+
+    switch (num_runs) {
+      case 2:
+      case 4:
+      case 6:
+        return true;
+    }
+
     if ($scope.scoreboard.left_bat.striker == true) {
       $scope.scoreboard.left_bat.striker = false;
       $scope.scoreboard.right_bat.striker = true;
@@ -41,33 +51,39 @@ angular.module('scorer', []).controller('ScorerController', function($scope) {
 
   $scope.bowls = function(type, runs) {
 
-    if (type == "wicket") {
-      $scope.scoreboard.wickets += 1;
-      var next_batsman_no = ($scope.scoreboard.left_bat.no > $scope.scoreboard.right_bat.no) ?
-        $scope.scoreboard.left_bat.no + 1 :
-        $scope.scoreboard.right_bat.no + 1;
-
-      if ($scope.scoreboard.left_bat.striker == true) {
-        $scope.scoreboard.left_bat = batsman;
-        $scope.scoreboard.left_bat.no = next_batsman_no;
-        $scope.scoreboard.left_bat.striker = true;
-
-      } else {
-        $scope.scoreboard.right_bat = batsman;
-        $scope.scoreboard.right_bat.no = next_batsman_no;
-        $scope.scoreboard.right_bat.striker = true;
-      }
+    if ($scope.scoreboard.game_over == true) {
+      alert("The innings is over!");
+      return false;
     }
 
-    $scope.scoreboard.total += runs;
-    $scope.scoreboard.balls++;
-
-    if ($scope.scoreboard.left_bat.striker) {
-      $scope.scoreboard.left_bat.runs += runs;
-    } else {
-      $scope.scoreboard.right_bat.runs += runs;
+    switch (type) {
+      case 'wicket':
+        wicket();
+        break;
+      case 'bye':
+      case 'leg_bye':
+        $scope.scoreboard.balls += 1;
+        change_ends(1);
+      case 'no_ball':
+      case 'wide':
+        $scope.scoreboard.extras += 1;
+        $scope.scoreboard.total += 1;
+        break;
+      case 'ball':
+        ball(runs);
+        change_ends(runs);
+        break;
     }
 
+    over();
+
+    if ($scope.scoreboard.last_innings > 0 && $scope.scoreboard.total > $scope.scoreboard.last_innings) {
+      $scope.scoreboard.game_over = true;
+    }
+
+  };
+
+  var over = function() {
     if ($scope.scoreboard.balls >= 6) {
       $scope.scoreboard.balls = 0;
       $scope.scoreboard.overs += 1;
@@ -76,19 +92,39 @@ angular.module('scorer', []).controller('ScorerController', function($scope) {
     } else {
       $scope.scoreboard.overs_and_balls = $scope.scoreboard.overs + '.' + $scope.scoreboard.balls;
     }
+  };
 
-    switch (runs) {
-      case 1:
-        change_ends();
-        break;
-      case 3:
-        change_ends();
-        break;
-      case 5:
-        change_ends();
-        break;
+  var ball = function(runs) {
+    $scope.scoreboard.total += runs;
+    $scope.scoreboard.balls++;
+
+    if ($scope.scoreboard.left_bat.striker) {
+      $scope.scoreboard.left_bat.runs += runs;
+    } else {
+      $scope.scoreboard.right_bat.runs += runs;
     }
+  }
 
+  var wicket = function() {
+    $scope.scoreboard.balls++;
+    $scope.scoreboard.wickets += 1;
+    var next_batsman_no = ($scope.scoreboard.left_bat.no > $scope.scoreboard.right_bat.no) ?
+      $scope.scoreboard.left_bat.no + 1 :
+      $scope.scoreboard.right_bat.no + 1;
+
+    if ($scope.scoreboard.left_bat.striker == true) {
+      $scope.scoreboard.left_bat = batsman;
+      $scope.scoreboard.left_bat.no = next_batsman_no;
+      $scope.scoreboard.left_bat.striker = true;
+
+    } else {
+      $scope.scoreboard.right_bat = batsman;
+      $scope.scoreboard.right_bat.no = next_batsman_no;
+      $scope.scoreboard.right_bat.striker = true;
+    }
+    if ($scope.scoreboard.wickets >= 10) {
+      $scope.scoreboard.game_over = true;
+    }
   };
 
 });
