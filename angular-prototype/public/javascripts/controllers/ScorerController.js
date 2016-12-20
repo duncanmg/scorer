80f070b481746b83990294a16df9f6cb637313df
Duncan Garland <duncan.garland@ntlworld.com>
2016-12-20 20:45:42 +0000

    The "Others" pages has buttons which increment the no balls as extras and/or runs. Changes can be accepted or rejects via glyphs, but the code isn't there yet.

--- angular-prototype/public/javascripts/controllers/ScorerController.js
+++ angular-prototype/public/javascripts/controllers/ScorerController.js
@@ -135,12 +135,41 @@ angular.module('scorer').controller('ScorerController', ['$scope', '$stateParams
     };
 
   }])
-  .controller('OthersController', ['$scope', '$stateParams', function($scope, $stateParams) {
+  .controller('OthersController', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state) {
     // alert('qqqq');
     $scope.no_ball_extras = 1;
     $scope.no_ball_runs = 0;
-    $scope.wide_extras=1;
-    $scope.wide_extras=0;
+    $scope.wide_extras = 1;
+    $scope.wide_extras = 0;
     $scope.leg_bye_extras = 1;
+
+    $scope.no_ball_extras_up = function() {
+      $scope.no_ball_extras += 1;
+    };
+    $scope.no_ball_extras_down = function() {
+      if ($scope.no_ball_extras) {
+        $scope.no_ball_extras -= 1;
+      }
+    };
+
+    $scope.no_ball_runs_up = function() {
+      $scope.no_ball_runs += 1;
+    };
+    $scope.no_ball_runs_down = function() {
+      if ($scope.no_ball_runs > 0) {
+        $scope.no_ball_runs -= 1;
+      }
+    };
+
+    $scope.accept = function() {
+      alert("ok");
+    };
+
+    $scope.reject = function() {
+      // alert("reject");
+      $state.go('scorer');
+    };
+
+
   }]);
 // alert('Bang!');
