9380ae16baa9e0fffa406db7d0e65983d1f80fd2
Duncan Garland <duncan.garland@ntlworld.com>
2017-01-18 21:50:25 +0000

    More work on history.

--- angular-prototype/js/controllers/HistoryController.js
+++ angular-prototype/js/controllers/HistoryController.js
@@ -9,8 +9,10 @@ angular.module('scorer').controller('HistoryController', ['$scope', '$stateParam
   // $scope.board.reset();
 
   //alert(JSON.stringify($scope.board));
-  //alert(JSON.stringify($scope.board.scoreboard.overs_history));
 
+
+  $scope.overs = board.scoreboard.overs_history.reverse();
+  alert(JSON.stringify($scope.overs[0].bowler));
   //$scope.bowlers = $scope.board.scoreboard.get_bowlers();
   //var bowlers_by_no = {};
   //for (var i = 0; i < $scope.bowlers.length; i++) {
--- angular-prototype/js/services/ScoreboardService.js
+++ angular-prototype/js/services/ScoreboardService.js
@@ -27,18 +27,21 @@ angular.module("scorer").factory('Scoreboard', ['Storage', 'Settings', '$rootSco
    * @param {integer} bowler_no - The number of the bowler
    * @return {Over} The new Over Object.
    *
-   * @property bowler_no
+   * @property bowler_obj
    * @property over_no
    * @property balls
    * @property valid_balls - Number of valid balls bowled.
    * @property total_balls - Number of balls bowled including extras.
    * @property add_ball - Add a ball this.add_ball(striker, runs, extras, wkt, valid)
    */
-  var Over = function(over_no, bowler_no) {
+  var Over = function(over_no, bowler_obj) {
     /** @property over_no */
     this.over_no = over_no;
-    this.bowler_no = bowler_no;
+    this.bowler = jQuery.extend({}, bowler_obj); // Shallow copy / clone.
     this.balls = [];
+    if (!over_no || !bowler_no) {
+      throw "Over object requires over_no and bowler_no";
+    }
     // this.add_ball = function(striker, runs, extras, wkt, valid) {
     //   this.balls.push({
     //     'striker': striker,
@@ -223,7 +226,8 @@ angular.module("scorer").factory('Scoreboard', ['Storage', 'Settings', '$rootSco
         this.scoreboard.overs_and_balls = this.scoreboard.overs;
         this.change_ends();
         this.change_bowlers();
-        this.add_over(this.scoreboard.overs, this.scoreboard.bowler);
+        alert("About to add over " + this.scoreboard.overs + 1);
+        this.add_over(this.scoreboard.overs + 1, this.scoreboard.bowler);
       } else {
         this.scoreboard.overs_and_balls = this.scoreboard.overs + '.' + this.scoreboard.balls;
       }
@@ -378,15 +382,15 @@ angular.module("scorer").factory('Scoreboard', ['Storage', 'Settings', '$rootSco
     /** @function */
     set_bowler_details: function() {
 
-      var get_bowlers = function(players) {
-        var bowling = [];
-        for (var i = 0; i < players.length; i++) {
-          if (players[i].bowling) {
-            bowling.push(players[i]);
-          }
-        }
-        return bowling;
-      };
+      // var get_bowlers = function(players) {
+      //   var bowling = [];
+      //   for (var i = 0; i < players.length; i++) {
+      //     if (players[i].bowling) {
+      //       bowling.push(players[i]);
+      //     }
+      //   }
+      //   return bowling;
+      // };
       /** @function */
       var is_bowling = function(bowlers, bowler) {
         for (var i = 0; i < bowlers.length; i++) {
@@ -415,8 +419,7 @@ angular.module("scorer").factory('Scoreboard', ['Storage', 'Settings', '$rootSco
 
       var bowling_team = this.scoreboard.batting_team == "home" ? this.away_players : this.home_players;
       var bowlers = Players.get_bowlers();
-      //alert(bowlers);
-      //alert("set_bowler: " + bowlers);
+
       this.scoreboard.bowler = set_bowler(bowlers, this.scoreboard.bowler);
 
       //alert("next set_bowler: " + bowlers.length + " : " + JSON.stringify(this.scoreboard.next_bowler));
@@ -436,18 +439,13 @@ angular.module("scorer").factory('Scoreboard', ['Storage', 'Settings', '$rootSco
       this.set_batsmen_details();
       this.set_bowler_details();
 
-      if (this.is_ready) {
-        if (!this.is_ready()) {
-          this.add_over();
-        }
-      }
     },
-    add_over: function(over_no, bowler_no) {
-      this.scoreboard.overs_history.push(new Over(over_no, bowler_no));
+    add_over: function(over_no, bowler_obj) {
+      this.scoreboard.overs_history.push(new Over(over_no, bowler_obj));
     },
     add_ball: function(striker, runs, extras, wkt, valid) {
       if (!this.scoreboard.overs_history.length) {
-        alert("Please add an over.");
+        this.add_over(1, this.scoreboard.bowler);
       }
       var over = this.scoreboard.overs_history[this.scoreboard.overs_history.length - 1];
       if (over.valid_balls >= 6) {
