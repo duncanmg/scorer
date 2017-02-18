1fce41e4cc96ff20e1db2dd20d21424dffce84cd
Duncan Garland <duncan.garland@ntlworld.com>
2017-02-18 18:56:09 +0000

    Dummy jasmine test file.

---
+++ angular-prototype/tests/unit/tests.js
@@ -0,0 +1,9 @@
+describe("dummy test", function() {
+  it("does something clever", function() {
+    expect(1).toEqual(1);
+  });
+  it("does something cleverer", function() {
+    expect(1+1).toEqual(2);
+  });
+});
+
