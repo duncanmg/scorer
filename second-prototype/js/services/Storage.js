6aa90f553218f008d45819482a136e5a83da651a
Duncan Garland <duncan.garland@ntlworld.com>
2018-04-06 12:22:54 +0100

    Made get() and put() available externally, added get_scoreboard().
    Remember that this is a factory not a service!

--- second-prototype/js/services/Storage.js
+++ second-prototype/js/services/Storage.js
@@ -6,9 +6,9 @@
  *     It will need to be extended to access a database and possibly synchronize
  *     remote and local storage.
  */
-angular.module("scorer").factory('Storage', function() {
+angular.module("scorer").factory('Storage', [function() {
 
-  return function () {
+  return function() {
 
     // Private function.
     var get = function(key) {
@@ -28,65 +28,73 @@ angular.module("scorer").factory('Storage', function() {
     };
 
     /**
-      * @function get_match
-      * @memberOf scorer.factory.Storage
-      * @param {integer} match_id - Unique identifier of the match.
-      */
+     * @function get_match
+     * @memberOf scorer.factory.Storage
+     * @param {integer} match_id - Unique identifier of the match.
+     */
     this.get_match = function(match_id) {
       return this.get(match_id);
     };
 
     /**
-      * @function put_match
-      * @memberOf scorer.factory.Storage
-      * @param {integer} match_id - Unique identifier of the match.
-      * @param {MatchData} match - The MatchData object.
-      */
+     * @function put_match
+     * @memberOf scorer.factory.Storage
+     * @param {integer} match_id - Unique identifier of the match.
+     * @param {MatchData} match - The MatchData object.
+     */
     this.put_match = function(match_id, match) {
       this.put(match_id, match);
       return true;
     };
 
     /**
-      * @function get_team
-      * @memberOf scorer.factory.Storage
-      * @param {integer} team_id - Unique identifier of the team.
-      */
+     * @function get_team
+     * @memberOf scorer.factory.Storage
+     * @param {integer} team_id - Unique identifier of the team.
+     */
     this.get_team = function(team_id) {
       return this.get(team_id);
     };
 
     /**
-      * @function put_team
-      * @memberOf scorer.factory.Storage
-      * @param {integer} team_id - Unique identifier of the team.
-      * @param {TeamData} match_id - The TeamData object.
-      */
+     * @function put_team
+     * @memberOf scorer.factory.Storage
+     * @param {integer} team_id - Unique identifier of the team.
+     * @param {TeamData} match_id - The TeamData object.
+     */
     this.put_team = function(team_id, team) {
       this.put(team_id, team);
       return true;
     };
 
     /**
-      * @function get_settings
-      * @memberOf scorer.factory.Storage
-      * @param {integer} match_id - Unique identifier of the set of settings.
-      */
+     * @function get_settings
+     * @memberOf scorer.factory.Storage
+     * @param {integer} match_id - Unique identifier of the set of settings.
+     */
     this.get_settings = function(settings_id) {
       return this.get(settings_id);
     };
 
     /**
-      * @function put_settings
-      * @memberOf scorer.factory.Storage
-      * @param {integer} settings_id - Unique identifier of the set of settings.
-      * @param {SettingsData} match_id - The SettingsData object.
-      */
+     * @function put_settings
+     * @memberOf scorer.factory.Storage
+     * @param {integer} settings_id - Unique identifier of the set of settings.
+     * @param {SettingsData} match_id - The SettingsData object.
+     */
     this.put_settings = function(settings_id, settings) {
       this.put(settings_id, settings);
       return true;
     };
 
+    this.get = get;
+    this.put = put;
+
+    this.get_scoreboard = function() {
+      return this.get('scoreboard');
+    };
+
+
   };
 
-});
+}]);
