f90024ddf5018dd31489235c7c1243993bdda05e
Duncan Garland <duncan.garland@ntlworld.com>
2018-04-11 12:55:24 +0100

    000007 - SC - Fix Unit Tests
    Point at second-prototype. It was picking up the wrong files.

--- second-prototype/karma.conf.js
+++ second-prototype/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '/home/duncan/scorer/angular-prototype/public',
+    basePath: '/home/duncan/scorer/second-prototype/public',
 
 
     // frameworks to use
