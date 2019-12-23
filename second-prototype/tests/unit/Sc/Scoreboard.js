e69871cc596c57b43ec2b479ac8109f3e46e8ce1
Duncan <duncan.garland@ntlworld.com>
2019-12-23 18:13:56 +0000

    Fixes

--- second-prototype/tests/unit/Sc/Scoreboard.js
+++ second-prototype/tests/unit/Sc/Scoreboard.js
@@ -1,20 +1,42 @@
 describe("ScScoreboardTest", function() {
-  beforeEach(module('scorer'));
+  beforeEach(module("scorer"));
 
-  var sc
+  var sc;
+  var template;
+  var players;
 
-  beforeEach(inject(function(Sc) {
-    console.log('ScScoreboardTest BBBBBBBBBBBBBBBBBB!!!!!!!!');
-    sc = Sc;
-  }));
+  beforeEach(
+    inject(function(Players) {
+      console.log("Inject Players " + JSON.stringify(Players));
+      players = {};
+    })
+  );
 
-  beforeEach(inject(function(Players) {
-    console.log('Inject Players ' + JSON.stringify(Players));
-  }));
+  beforeEach(
+    inject(function(ScoreboardTemplate) {
+      // console.log('Inject Players ' + JSON.stringify(Players));
+      template = { innings: [{}, {}] };
+    })
+  );
 
-  it("An Sc.Scoreboard object has been created.",
-    function() {
-      expect(typeof(scoreboard)).toEqual('object');
-    });
+  beforeEach(
+    inject(function(Sc) {
+      console.log("ScScoreboardTest BBBBBBBBBBBBBBBBBB!!!!!!!!");
+      sc = Sc;
+    })
+  );
 
+  it("An Sc.Scoreboard object has been created.", function() {
+    //console.log("CCCCCCCCCCCCCCCCCCCCCCCCC");
+    expect(typeof sc).toEqual("object");
+    //console.log(JSON.stringify(sc));
+    //console.log("2 CCCCCCCCCCCCCCCCCCCCCCCCC");
+    expect(typeof sc.Scoreboard).toEqual("function");
+    //console.log("3 CCCCCCCCCCCCCCCCCCCCCCCCC");
+    expect(typeof template).toEqual("object");
+    //console.log("4 CCCCCCCCCCCCCCCCCCCCCCCCC");
+    expect(typeof players).toEqual("object");
+    //console.log("5 CCCCCCCCCCCCCCCCCCCCCCCCC");
+    sc2 = new sc.Scoreboard(template, players);
+  });
 });
