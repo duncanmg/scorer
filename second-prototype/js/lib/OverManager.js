d3899fe7bcab46a88fe55f9d76100228aaf73d6d
Duncan <duncan.garland@ntlworld.com>
2020-03-29 15:22:07 +0100

    OverManager is now quite robust.

--- second-prototype/js/lib/OverManager.js
+++ second-prototype/js/lib/OverManager.js
@@ -4,14 +4,16 @@ sc.OverManager = function(data) {
   console.log("OverManager data " + JSON.stringify(data));
   this.data = data;
 
-  var ok = true;
   if (typeof this.data.overs_history != "object") {
-    console.log("data.overs_history must be an array");
-    ok = false;
+    throw new Error("data.overs_history must be an array");
   }
 
-  if (!ok) {
-    throw new Error("new sc.OverManager failed");
+  if (typeof this.data.templates != "object") {
+    throw new Error("data.templates must be an object");
+  }
+
+  if (typeof this.data.templates.Ball != "function") {
+    throw new Error("data.templates.Ball must be a function");
   }
 
   /** @function add_ball
@@ -24,32 +26,82 @@ sc.OverManager = function(data) {
    *  @param valid
    */
   this.add_ball = function(striker, runs, extras, wkt, valid) {
-    console.log("add_ball " + JSON.stringify(this.data));
+    //console.log("add_ball " + JSON.stringify(this.data));
+    //console.log("runs=" + runs);
+    if (arguments.length != 5) {
+      throw new Error(
+        "OverManager.add_ball requires five arguments. Got " + arguments.length
+      );
+    }
+
+    if (typeof striker != "object") {
+      throw new Error(
+        "OverManager.add_ball argument 1 must be an object. Got " +
+          typeof striker
+      );
+    }
+
+    if (!(wkt == 0 || wkt == 1)) {
+      throw new Error("OverManager.add_ball wkt must be 0 or 1");
+    }
+
+    if (!(valid == 0 || valid == 1)) {
+      throw new Error("OverManager.add_ball valid must be 0 or 1");
+    }
+
+    if (runs !== parseInt(runs)) {
+      throw new Error("OverManager.add_ball run must be an integer");
+    }
+
+    if (extras !== parseInt(extras)) {
+      throw new Error("OverManager.add_ball errors must be an integer");
+    }
+
+    if (typeof striker != "object") {
+      throw new Error(
+        "OverManager.add_ball argument 1 must be an object. Got " +
+          typeof striker
+      );
+    }
     if (!this.data.overs_history.length) {
       // alert("xxxxx " + this.add_over);
       this.add_over(1, this.data.bowler);
     }
-    alert(this.data.overs_history.length);
+
     var over = this.data.overs_history[this.data.overs_history.length - 1];
     if (over.valid_balls >= 6) {
       alert("The over has finished.");
+      return;
     }
+
     this.data.overs_history[this.data.overs_history.length - 1].balls.push(
       new data.templates.Ball(striker, runs, extras, wkt, valid)
     );
+
     if (valid) {
       this.data.overs_history[
         this.data.overs_history.length - 1
       ].valid_balls += 1;
     }
+
     this.data.overs_history[
       this.data.overs_history.length - 1
     ].total_balls += 1;
   };
 
   this.add_over = function(over_no, bowler_obj) {
-    //console.log("add_over " + over_no);
-    //console.log("bowler_obj " + JSON.stringify(bowler_obj));
+    if (arguments.length != 2) {
+      throw new Error("OverManager add_over requires two arguments");
+    }
+
+    if (over_no !== parseInt(over_no)) {
+      throw new Error("OverManager add_over. over_no must be an integer");
+    }
+
+    if (typeof bowler_obj != "object") {
+      throw new Error("OverManager add_over. bowler_obj must be an object");
+    }
+
     this.data.overs_history.push(
       new this.data.templates.Over(over_no, bowler_obj)
     );
--- second-prototype/tests/unit/Sc/OverManager.js
+++ second-prototype/tests/unit/Sc/OverManager.js
@@ -5,6 +5,7 @@ describe("ScOverManagerTest Simple Construction", function() {
   var template;
   var players;
   var om;
+  var om_class;
 
   beforeEach(
     inject(function(Players) {
@@ -15,181 +16,90 @@ describe("ScOverManagerTest Simple Construction", function() {
 
   beforeEach(
     inject(function(ScoreboardTemplate) {
-      // console.log('Inject Players ' + JSON.stringify(Players));
-      template = { innings: [{}, {}] };
+      //console.log(
+      //  "Inject ScoreboardTemplate " + JSON.stringify(ScoreboardTemplate)
+      //);
+      template = new ScoreboardTemplate();
+      //console.log("template " + JSON.stringify(template));
     })
   );
 
   beforeEach(
     inject(function(Sc) {
-      console.log("ScOverManagerTest AAAAA");
-      sc = Sc;
-      // om = Sc.OverManager( {});
+      //console.log("ScOverManagerTest AAAAA");
+      sc = new Sc.Scoreboard(template, players);
+      //console.log(JSON.stringify(sc.scoreboard));
+      om = new Sc.OverManager(sc.scoreboard);
+      om_class = Sc.OverManager;
     })
   );
 
   it("Sc.OverManager constructor errors.", function() {
-    console.log("CCCCCCCCCCCCCCCCCCCCCCCCC");
-    expect(sc.OverManager).toBeDefined();
-    expect( function(){new sc.OverManager({});}).toThrow(new Error("new sc.OverManager failed"));
+    expect(om_class).toBeDefined();
 
-    //expect(typeof om).toEqual("object");
-    // expect(om).toBeInstanceOf(sc.OverManager);
+    // Missing overs_history
+    expect(function() {
+      new om_class({});
+    }).toThrow(new Error("data.overs_history must be an array"));
+
+    // Missing templates
+    expect(function() {
+      new om_class({ overs_history: [] });
+    }).toThrow(new Error("data.templates must be an object"));
+
+    // Missing templates.Ball
+    expect(function() {
+      new om_class({ overs_history: [], templates: {} });
+    }).toThrow(new Error("data.templates.Ball must be a function"));
   });
 
-  //it("An Sc.OverManager add_ball.", function() {
-//    var om = new sc.OverManager({});
-  //  expect(typeof om).toEqual("object");
-    //console.log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
-  //});
+  it("An Sc.OverManager add_ball arguments.", function() {
+    expect(typeof om).toEqual("object");
 
+    expect(function() {
+      om.add_ball();
+    }).toThrow(
+      new Error("OverManager.add_ball requires five arguments. Got 0")
+    );
 
+    expect(function() {
+      om.add_ball(1);
+    }).toThrow(
+      new Error("OverManager.add_ball requires five arguments. Got 1")
+    );
+
+    expect(function() {
+      om.add_ball(1, 2, 3, 4, 5, 6);
+    }).toThrow(
+      new Error("OverManager.add_ball requires five arguments. Got 6")
+    );
+
+    expect(function() {
+      om.add_ball(1, 2, 3, 4, 5);
+    }).toThrow(
+      new Error("OverManager.add_ball argument 1 must be an object. Got number")
+    );
+  });
+
+  it("An Sc.OverManager add_over.", function() {
+    expect(typeof om).toEqual("object");
+
+    expect(om.data.overs_history.length).toEqual(0);
+
+    expect(function() {
+      om.add_over();
+    }).toThrow(new Error("OverManager add_over requires two arguments"));
+
+    expect(function() {
+      om.add_over("a", "b");
+    }).toThrow(new Error("OverManager add_over. over_no must be an integer"));
+
+    expect(function() {
+      om.add_over(1, "b");
+    }).toThrow(new Error("OverManager add_over. bowler_obj must be an object"));
+
+    expect(om.data.overs_history.length).toEqual(0);
+    om.add_over(1, {});
+    expect(om.data.overs_history.length).toEqual(1);
+  });
 });
-
-//
-// describe("ScScoreboardTest. Real Template", function() {
-//   beforeEach(module("scorer"));
-//
-//   var sc;
-//   var template;
-//   var players;
-//   var over;
-//
-//   beforeEach(
-//     inject(function(Players) {
-//       console.log("Inject Players " + JSON.stringify(Players));
-//       players = {};
-//     })
-//   );
-//
-//   beforeEach(
-//     inject(function(ScoreboardTemplate) {
-//       template = new ScoreboardTemplate();
-//     })
-//   );
-//
-//   beforeEach(
-//     inject(function(Sc) {
-//       sc = Sc;
-//     })
-//   );
-//
-//   beforeEach(
-//     inject(function(Over) {
-//       over = Over;
-//     })
-//   );
-//
-//   it("An Sc.Scoreboard object has been created.", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//   });
-//
-//   it("An Sc.Scoreboard has overs_history and add_over.", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//     // console.log("sc2.scoreboard.overs_history " + JSON.stringify(sc2.scoreboard.overs_history));
-//     // console.log(sc2.scoreboard.overs_history.length);
-//     expect(sc2.scoreboard.overs_history.length).toEqual(0);
-//     sc2.add_over(1, 2);
-//     expect(sc2.scoreboard.overs_history.length).toEqual(1);
-//   });
-//
-//   it("Sc.Command set_innings_over", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//
-//     sc2.scoreboard.wickets = 9;
-//     var o = new sc.Command();
-//     o.data = sc2.scoreboard;
-//
-//     o.set_innings_over();
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     sc2.scoreboard.wickets = 10;
-//     o.set_innings_over();
-//     expect(sc2.scoreboard.innings_over).toEqual(true);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//   });
-//
-//   it("Sc.Command set_game_over. Not over", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//
-//     expect(sc2.scoreboard.last_innings).toEqual(0);
-//     expect(sc2.scoreboard.total).toEqual(0);
-//     expect(sc2.scoreboard.innings_no).toEqual(1);
-//
-//     sc2.scoreboard.wickets = 9;
-//     var o = new sc.Command();
-//     o.data = sc2.scoreboard;
-//
-//     expect(o.set_game_over()).toEqual(false);
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//   });
-//
-//   it("Sc.Command set_game_over. Batting team win.", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//
-//     expect(sc2.scoreboard.last_innings).toEqual(0);
-//     expect(sc2.scoreboard.total).toEqual(0);
-//     expect(sc2.scoreboard.innings_no).toEqual(1);
-//
-//     sc2.scoreboard.wickets = 9;
-//     sc2.scoreboard.last_innings = 100;
-//     sc2.scoreboard.total = 101;
-//     sc2.scoreboard.innings_no = 2;
-//
-//     var o = new sc.Command();
-//     o.data = sc2.scoreboard;
-//
-//     expect(o.set_game_over()).toEqual(true);
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(true);
-//   });
-//
-//   it("Sc.Command set_game_over. Bowling team win.", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//
-//     expect(sc2.scoreboard.innings_over).toEqual(false);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//     expect(sc2.scoreboard.innings_no).toEqual(1);
-//
-//     expect(sc2.scoreboard.last_innings).toEqual(0);
-//     expect(sc2.scoreboard.total).toEqual(0);
-//
-//     sc2.scoreboard.wickets = 10;
-//     sc2.scoreboard.last_innings = 100;
-//     sc2.scoreboard.total = 90;
-//     sc2.scoreboard.innings_no = 2;
-//
-//     var o = new sc.Command();
-//     o.data = sc2.scoreboard;
-//
-//     expect(o.set_game_over()).toEqual(true);
-//     expect(sc2.scoreboard.innings_over).toEqual(true);
-//     expect(sc2.scoreboard.game_over).toEqual(true);
-//   });
-//
-//   it("An Sc.Commands.Wicket", function() {
-//     sc2 = new sc.Scoreboard(template, players, over);
-//     expect(typeof sc2).toEqual("object");
-//     expect(sc2.scoreboard.wickets).toEqual(0);
-//     expect(sc2.scoreboard.balls).toEqual(0);
-//     sc2.wicket();
-//     expect(sc2.scoreboard.wickets).toEqual(1);
-//     expect(sc2.scoreboard.balls).toEqual(1);
-//     expect(sc2.scoreboard.game_over).toEqual(false);
-//   });
-//});
