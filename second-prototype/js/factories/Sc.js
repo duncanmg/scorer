e96b8c9bcceb304c855d3c79397784b94122f81e
Duncan <duncan.garland@ntlworld.com>
2020-08-23 16:03:46 +0100

    Give logger access to $http.

--- second-prototype/js/factories/Sc.js
+++ second-prototype/js/factories/Sc.js
@@ -3,21 +3,24 @@
  * https://www.jvandemo.com/how-to-properly-integrate-non-angularjs-libraries-in-your-angularjs-application/
  */
 
-angular.module("scorer").factory('Sc', ['$window', function($window) {
+angular.module("scorer").factory('Sc', ['$window', '$http',
+  function($window, $http) {
 
-  /** Creates an instance of Sc
-   *
-   * @class Sc
-   * @memberOf scorer.factory
-   * @description Factory which returns an Sc object. Properly integrates
-   * lib/sc.js
-   * @constructor Sc
-   */
+    /** Creates an instance of Sc
+     *
+     * @class Sc
+     * @memberOf scorer.factory
+     * @description Factory which returns an Sc object. Properly integrates
+     * lib/sc.js
+     * @constructor Sc
+     */
 
-  if (window.sc) {
-    return window.sc;
-  } else {
-    console.log('ERROR window.sc is not available.');
+    if (window.sc) {
+      window.sc.LoggerHttp = $http;
+      return window.sc;
+    } else {
+      console.log('ERROR window.sc is not available.');
+    }
+
   }
-
-}]);
+]);
