4981a83b994f594ba8b9b1c2837df8b11fd0aed9
Duncan Garland <duncan.garland@ntlworld.com>
2020-09-12 10:57:28 +0000

    Made logging dependent on an environment variable. Worried about DOS attacks on live.

--- second-prototype/routes/log.js
+++ second-prototype/routes/log.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* POST a log message. */
+/* POST a log message if the environment variable SCORERLOGGING is true. */
 router.post('/', function(req, res, next) {
-	var date = new Date();
-	var day = date.getDate();
-	fs = require('fs');
-	var data = JSON.stringify(req.body.msg);
-	fs.appendFile('/home/duncan/dev_scorer/second-prototype/logs/log'+day+'.log', data + "\n", 'utf8', function(err) { console.log(err);});
+  var date = new Date();
+  var day = date.getDate();
+  if (process.env.SCORERLOGGING) {
+    fs = require('fs');
+    var data = date.toUTCString() + ': ' + JSON.stringify(req.body.msg);
+    fs.appendFile('/home/duncan/dev_scorer/second-prototype/logs/log' + day + '.log', data + "\n", 'utf8', function(err) {
+      console.log(err);
+    });
+  }
   res.send('ok with a resource');
 });
 
